36ee7f49e821ac1d0ea711fafdbe072bd4f5223e
ajithkmr997 <31098135+ajithkmr997@users.noreply.github.com>
2017-08-17 16:59:55 +0530

    c program

---
+++ positive.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+int a;
+printf('Enter the value:");
+scanf("&d",&a);
+if(a>0)
+printf("The value is positive");
+if(a=0)
+printf("The value is zero");
+if(a<0)
+printf("The value is negative)
+}
